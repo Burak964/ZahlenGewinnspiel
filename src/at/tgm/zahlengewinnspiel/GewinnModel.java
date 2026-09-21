99d535078a649ccdcc2ba3e066e667ddacd5917b
btopcu <mehmudburak66@gmail.com>
2026-09-21 11:08:50 +0200

    Add model attributes and getters

--- src/at/tgm/zahlengewinnspiel/GewinnModel.java
+++ src/at/tgm/zahlengewinnspiel/GewinnModel.java
@@ -1,4 +1,24 @@
 package at.tgm.zahlengewinnspiel;
 
 public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    public GewinnModel() {
+        gesamtPunkte = 30;
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
 }
